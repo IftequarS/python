a98c878104f2a1d6fa4652baf5469976ae5318c8
IftequarS <70209590+IftequarS@users.noreply.github.com>
2022-07-06 23:52:53 +0530

    Add files via upload

---
+++ strings (1).py
@@ -0,0 +1,21 @@
+#slicing and indexing mix 
+index_me = "iftequar"
+char = index_me[0:8] , index_me[7], index_me[6]
+print(index_me[0:8])
+print(char)
+#slicing
+char = 'hello'
+print(char[3:])
+#indexing
+word = "practicing strings"
+a = word[int(input())]
+print(a)
+#doubt can input function be used to perform slicing  
+'''spam = "Free money"
+cash = spam[int(input())]
+print(cash)'''
+#string concatenation
+name = "Jftequar"
+word = name[1:]
+print(word)
+print("I" + word)
