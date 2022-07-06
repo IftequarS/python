1b41fda8b72b3f8654b0f0ca588c05d2db651f64
IftequarS <70209590+IftequarS@users.noreply.github.com>
2022-07-06 23:55:05 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+#addition of two numbers 
+num1 = 515
+num2 = 128
+sum = num1+num2
+print("The sum of 515 + 128 is " ,sum)
+#subtraction of two numbers
+a = 1024
+b = 512
+sub = a-b
+print("The Subtraction of 1024-512 is", sub)
+#multiplication of two numbers
+num1 = int(input("Enter a number1: "))
+num2 = int(input("Enter a number2: "))
+mutli = num1*num2
+print("The multiplication of given input is ", mutli )
