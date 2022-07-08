2b78f8f3a8e89c5b03f28521a8fed70d640527b8
IftequarS <70209590+IftequarS@users.noreply.github.com>
2022-07-09 00:17:26 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -42,3 +42,16 @@ tuple = (11,22,33,44,55,66)'''
 tuple = (11,22,33,44,55,66)
 new_tuple = tuple[3 : -1]
 print(new_tuple)
+
+'''7.Modify the tuple.
+Given is a nested tuple. Write a program to modify the first item (22) of a list inside a following tuple to 222
+tuple1 = (11, [22, 33], 44, 55)'''
+tuple1 = (11, [22, 33], 44, 55)
+t = tuple1[1]
+a = t[0] = 222
+print(tuple1)
+
+'''8.Counts the number of occurrences of item 50 from a tuple
+tpl = (50, 10, 60, 70, 50)'''
+tpl = (50, 10, 60, 70, 50)
+print(tpl.count(50))
