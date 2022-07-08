dc3c456e53654f77c52d41e7f67798695f589ec6
IftequarS <70209590+IftequarS@users.noreply.github.com>
2022-07-08 23:47:29 +0530

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -46,3 +46,21 @@ if 20 in d:
     print("20 is present in dict")
 else:
     print("does not present")
+    
+'''7.Get the key of a minimum value from the following dictionary
+sample_dict = {'Physics': 82,'Math': 65,'history': 75}'''
+sample_dict = {'physics': 82, 'math': 65, 'history': 75}
+###########
+print('6.',min(sample_dict, key=sample_dict.get))
+
+'''8.Change value of a key in a nested dictionary
+Write a Python program to change Brad’s salary to 8500 in the following dictionary.
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}'''
+sample_dict = {'emp1' : {'name': 'Jhon', 'salary': 7500 },'emp2': {'name': 'emma', 'salary': 8000 },
+'emp3': {'name' : 'brad', 'salary' : 500}}
+sample_dict['emp3']['salary'] =8500
+print('7.',sample_dict)
